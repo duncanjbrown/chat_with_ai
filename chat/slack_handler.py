a4772f6cc7ce6fcccb04f131607a067bcbc378ef
Duncan Brown <duncanjbrown@gmail.com>
2023-08-30 10:55:41 +0100

    Add llm, serve completions

--- chat/slack_handler.py
+++ chat/slack_handler.py
@@ -1,9 +1,14 @@
 import logging
 import os
+import re
+import llm
 
 from slack_bolt import App
 
 logger = logging.getLogger(__name__)
+
+model = llm.get_model("gpt-3.5-turbo")
+model.key = os.getenv('OPENAI_API_KEY')
 
 app = App(
     token=os.environ["SLACK_BOT_TOKEN"],
@@ -15,5 +20,12 @@ app = App(
 
 @app.event("app_mention")
 def handle_app_mentions(logger, event, say):
+    text = event['text']
+
+    # this will not do well with mentions in the middle of the string
+    cleaned_text = re.sub(r'<[^>]+>', '', text).strip()
+
+    response = model.prompt(cleaned_text)
+
     logger.info(event)
-    say(f"Hi there, <@{event['user']}>")
+    say(f"<@{event['user']}>\n\n*{cleaned_text}*.\n\n_{response.text()}_")
